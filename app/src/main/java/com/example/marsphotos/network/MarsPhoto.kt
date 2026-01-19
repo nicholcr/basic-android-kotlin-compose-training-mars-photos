29d98b1da6a8512cece762e9c0ebc3d98ca81be1
nicholcr <nicholcr@umich.edu>
2026-01-19 11:21:21 -0600

    Implement repository and manual dependency injection for the viewmodel

--- app/src/main/java/com/example/marsphotos/network/MarsPhoto.kt
+++ app/src/main/java/com/example/marsphotos/network/MarsPhoto.kt
@@ -3,6 +3,9 @@ package com.example.marsphotos.network
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
+/**
+ * This data class defines a Mars photo which includes an ID, and the image URL.
+ */
 @Serializable
 data class MarsPhoto(
     val id: String,
